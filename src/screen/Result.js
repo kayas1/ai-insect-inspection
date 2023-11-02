c7f4807d1d4f2ebd0fdac1f4dc4f7c050c4b46ee
kayas1 <wogur1833@naver.com>
2023-11-02 18:01:08 +0900

    .

--- src/screen/Result.js
+++ src/screen/Result.js
@@ -5,12 +5,36 @@ import baseStyles from "../styles/Main.module.css";
 import resultImg from "../tempFile/Helicoverpa_assulta_byNCPMS.JPG";
 import resultChart from "../tempFile/result.jpg";
 import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import Chart from 'react-apexcharts'
+
+class Donut extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {labels: ['검거세미밤나방', '담배거세미나방', '담배나방', '도둑나방', '기타'],dataLabels:{style:{fontSize:'20px'}}},
+      series: [92, 3, 3, 1, 1],
+      
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="donut">
+        <Chart options={this.state.options} series={this.state.series} type="donut" width="500" />
+      </div>
+    );
+  }
+}
 
 function MainBlock(){
     return(
         <div className={styles.mainContainer}>
             <img className={styles.resultImg} src={resultImg} alt=""/>
-            <img className={styles.resultImg} src={resultChart} alt=""/>
+            <Donut/>
             <div className={styles.btnsContainer}><Link to="/insectinfo">
             <button className={styles.moreInfoBtn}>해충 상세 정보</button>
             </Link>
