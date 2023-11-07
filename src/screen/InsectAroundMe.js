6e6558f68d873e9a1362a3cb9967fe83d2c5dfdc
kayas1 <wogur1833@naver.com>
2023-11-07 23:55:53 +0900

    .

--- src/screen/InsectAroundMe.js
+++ src/screen/InsectAroundMe.js
@@ -8,37 +8,33 @@ import {
   useNavermaps,
 } from "react-naver-maps";
 
-function NaverMapApi() {
+function Map() {
   function MyMap() {
     const navermaps = useNavermaps();
-
+    let lat = 37.3595704;
+    let lng = 127.105399;
+    navigator.geolocation.getCurrentPosition((position) => {
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+    }, alert("권한을 허락해주십시오"));
     return (
-      <NaverMap
-        defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)}
-        defaultZoom={15}
-      >
-        <Marker position={new navermaps.LatLng(37.3595704, 127.105399)} />
+      <NaverMap defaultCenter={new navermaps.LatLng(lat, lng)} defaultZoom={15}>
+        <Marker position={new navermaps.LatLng(lat, lng)} />
       </NaverMap>
     );
   }
   return (
-    <MapDiv
-      style={{
-        width: "100%",
-        height: "600px",
-      }}
-    >
-      <MyMap />
-    </MapDiv>
-  );
-}
-
-function Map() {
-  return (
     <div className="map-loader">
       <div className="map">
         <NavermapsProvider ncpClientId={process.env.MAPCLIENTID}>
-          <NaverMapApi />
+          <MapDiv
+            style={{
+              width: "100%",
+              height: "600px",
+            }}
+          >
+            <MyMap />
+          </MapDiv>
         </NavermapsProvider>
       </div>
     </div>
